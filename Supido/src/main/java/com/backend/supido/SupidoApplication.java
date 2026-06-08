69f15ec329ab4a664d31a212e5de87d48898b070
Andrea-004 <00106123@uca.edu.sv>
2026-06-07 18:10:47 -0600

    feat - initial backend structure

---
+++ Supido/src/main/java/com/backend/supido/SupidoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.supido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupidoApplication.class, args);
+    }
+
+}
---
+++ Supido/src/test/java/com/backend/supido/SupidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.supido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
