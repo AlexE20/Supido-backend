9f5e1c33323fecc252aad3df9aa160228dbf82ad
Andrea-004 <00106123@uca.edu.sv>
2026-06-08 11:25:45 -0600

    feat - adding restaurant and menuItem repositories

---
+++ src/main/java/com/backend/supido/repositories/restaurant/RestaurantRepository.java
@@ -0,0 +1,13 @@
+package com.backend.supido.repositories.restaurant;
+
+import com.backend.supido.domain.entities.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    List<Restaurant> findByCategory(String category);
+    List<Restaurant> findByNameContainingIgnoreCase(String name);
+}
