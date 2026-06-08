ee375f947a833b766012179c40e90bb3eca94017
AlexE20 <00009423@uca.edu.sv>
2026-06-08 02:08:32 -0600

    feat:Adding role and user repository

--- src/main/java/com/backend/supido/repositories/RoleRepository.java
+++ src/main/java/com/backend/supido/repositories/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.backend.supido.repositories;
 
-public class RoleRepository {
+import com.backend.supido.domain.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
 }
