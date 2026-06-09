6255bcd1059e2cd4ea73b09cdea4e32bfafd245d
AlexE20 <00009423@uca.edu.sv>
2026-06-09 01:56:12 -0600

    feat:Creating DTOS

--- src/main/java/com/backend/supido/auth/domain/dto/request/RegisterRequest.java
+++ src/main/java/com/backend/supido/auth/domain/dto/request/RegisterRequest.java
@@ -1,4 +1,29 @@
 package com.backend.supido.auth.domain.dto.request;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "El nombre de usuario es obligatorio.")
+    private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria.")
+    private String password;
+
+    @NotBlank(message = "El correo electrónico es obligatorio.")
+    @Email(message = "El correo electrónico no es válido.")
+    private String email;
+
+    @NotBlank(message = "El telefono celular es obligatorio.")
+    private String phone;
+
+
+
+
 }
--- src/main/java/com/backend/supido/auth/domain/dto/response/AuthResponse.java
+++ src/main/java/com/backend/supido/auth/domain/dto/response/AuthResponse.java
@@ -1,4 +1,12 @@
 package com.backend.supido.auth.domain.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
+    private String token;
 }
