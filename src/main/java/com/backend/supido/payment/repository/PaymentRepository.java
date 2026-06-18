488b97200bb99de8f5c9c20eea171d0e751b9130
robnez44 <00044623@uca.edu.sv>
2026-06-18 02:48:28 -0600

    feat: payment repository

---
+++ src/main/java/com/backend/supido/payment/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.backend.supido.payment.repository;
+
+import com.backend.supido.payment.domain.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(Long orderId);
+}
