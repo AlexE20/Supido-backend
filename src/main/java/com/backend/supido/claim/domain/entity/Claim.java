dc775665cda0b6c43381f09e2bc98fcaf6c7639b
robnez44 <00044623@uca.edu.sv>
2026-06-19 16:20:53 -0600

    feat: claim entinty, and enum types

---
+++ src/main/java/com/backend/supido/claim/domain/entity/Claim.java
@@ -0,0 +1,44 @@
+package com.backend.supido.claim.domain.entity;
+
+import com.backend.supido.claim.domain.enums.ClaimStatus;
+import com.backend.supido.claim.domain.enums.ClaimType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "claims")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Claim {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private ClaimType type;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private ClaimStatus status;
+
+    @Column(name = "refund_amount")
+    private BigDecimal refundAmount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
